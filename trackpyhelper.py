c6f27b97bdfdd951c99bd08a6119345abd1ae049
UChicagoPhysicsLabs <55201443+UChicagoPhysicsLabs@users.noreply.github.com>
2021-11-05 12:37:41 -0500

    Readability fix
    Removed separation option as it wasn't helping students.  Added color-coding above cells to better identify when parameters should be altered.

--- trackpyhelper.py
+++ trackpyhelper.py
@@ -1,4 +1,4 @@
 import trackpy as tp
-def batch(frames,d,mm,sepa):
-	a= tp.batch(frames, diameter=d, minmass=mm, invert=True,processes=1,separation=sepa)
+def batch(frames,d,mm):
+	a= tp.batch(frames, diameter=d, minmass=mm, invert=True,processes=1)
 	return a
